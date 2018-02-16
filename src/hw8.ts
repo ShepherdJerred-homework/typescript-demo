401b399be5d1adad2e4581bc5b3a060464c8362d
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-02-16 12:53:39 -0600

    Skeleton for more classes and functions

--- src/hw8.ts
+++ src/hw8.ts
@@ -8,4 +8,35 @@ export function doubleAll (a: number[]): number[] {
   });
 }
 
+export function mapSum (a: string[], f: Function): number {
 
+}
+
+export interface Location {
+  x: number;
+  y: number;
+}
+
+export class Circle {
+  x: number;
+  y: number;
+  radius: number;
+
+  constructor (radius: number) {
+    this.x = 0;
+    this.y = 0;
+    this.radius = radius;
+  }
+
+  location (): Location {
+
+  }
+
+  area (): number {
+
+  }
+
+  moveTo(location: Location) {
+
+  }
+}
