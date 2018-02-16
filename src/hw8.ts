898cbf5b561766d9c2137194309957616927b274
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-02-16 12:42:20 -0600

    Add doubleAll

--- src/hw8.ts
+++ src/hw8.ts
@@ -1,3 +1,11 @@
-function sayHello (name: string): string {
+export function sayHello (name: string): string {
   return 'Hello, ' + name;
 }
+
+export function doubleAll (a: number[]): number[] {
+  return a.map(function (n): number {
+    return n * 2;
+  });
+}
+
+
